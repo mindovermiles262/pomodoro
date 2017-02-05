d35daa1956680c2de14d71bef182c29b06f72570
Erik Johnson <mindovermiles262@gmail.com>
2017-02-04 19:31:25 -0800

    semi-working, timer display not updating

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-	var work = 25
+	var work = 4
 	var rest = 5
 	var reps = 2
-	var timer = "00:00"
-	
+	var min = work
+	var sec = 1
+
+
 // Display work, rest, reps, and time
 	disp_work(work);
 	disp_rest(rest);
 	disp_reps(reps);
-	disp_timer(timer);
+	disp_timer(min, sec);
 
 // add or remove work time
 	$('#work_minus').click( function() {
@@ -17,6 +19,8 @@ $(document).ready(function() {
 		work = work - 1;
 		} else (work = 1);
 		disp_work(work);
+		timer = new Date(work);
+		disp_timer(timer, sec)
 	})
 	$('#work_plus').click( function() {
 		$('#work_count').empty();
@@ -24,6 +28,8 @@ $(document).ready(function() {
 			work = work + 1
 		} else (work = 60);
 		disp_work(work);
+		timer = new Date(work);
+		disp_timer(timer, sec)
 	})
 
 // add or remove rest time
@@ -58,6 +64,23 @@ $(document).ready(function() {
 		disp_reps(reps);
 	})
 
+// click start button, start timer
+	$('#start').click( function() {
+		var start_time = new Date();
+		var add_time = minutes_to_milliseconds(work);
+		var work_end_time = new Date(start_time.getTime() + add_time);
+		add_time = minutes_to_milliseconds(rest);
+		var rest_end_time = new Date(work_end_time.getTime() + add_time);
+		timer = work_end_time;
+		do {
+			var time_left = work_end_time.getTime() - new Date();
+			min = new Date(time_left).getMinutes();
+			sec = new Date(time_left).getSeconds();
+			disp_timer(min, sec);
+		}	while (new Date() < work_end_time);
+		alert("Not in do/while loop!");
+	})
+
 });
 
 // functions to display work, rest, rep, timer numbers
@@ -70,6 +93,23 @@ var disp_rest = function(min) {
 var disp_reps = function(repetitions) {
 	$('#reps_count').append(repetitions);
 }
-var disp_timer = function(time) {
-	$('#timer_count').append(time);
+var disp_timer = function(min, sec) {
+	min = minutes_to_milliseconds(min);
+	sec = sec * 1000;
+	$('#timer_count').empty();
+	$('#timer_count').append(addZero(new Date(min).getMinutes()) + ":" + addZero(new Date(sec).getSeconds()));
+}
+
+// convert minutes to milliseconds
+var minutes_to_milliseconds = function(min) {
+	var ms = min * 60 * 1000;
+	return ms;
+}
+
+// add zeros for time display
+var addZero = function(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
