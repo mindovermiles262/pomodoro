b2772e6f5ae587b0d4df3c5601f3a847a2f72d31
Erik Johnson <mindovermiles262@gmail.com>
2017-02-13 21:10:17 -0800

    update 2017.02.13

---
+++ README.md
@@ -0,0 +1,26 @@
+#Pomodoro Clock
+The Pomodoro Technique is a time management process in which segments of time are divided into work and break. Typically 25 minutes of work is followed by 5 minutes of rest.  This timer allows users to set the time intervals they want. After the work timer has completed, a rest timer will automatically begin. Live version of this timer can be found [here](http://andyduss.com/pomodoro)
+
+##Use
+To use, simply adjust work and rest time using the (-) and (+) buttons then click start
+
+##Development Languages
+* HTML
+* CSS
+* jQuery/Javascript
+
+##TODO:
+1. Program Stop button
+2. Add reset functionality
+3. Add sound on finished
+4. Add pie slice shading
+5. Mobile version
+
+##Copyright Notice
+Copyright 2017 Andy Duss
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- js/script.js
+++ js/script.js
@@ -1,161 +1,107 @@
-$(document).ready(function() {
-	var work = 25
-	var rest = 5
-	var reps = 6
-	var min = work
-	var sec = 0
+// Helper Functions
 
+// display work and rest time selectors
+var disp_work = function(ms) {
+  min = Math.floor(ms/60000);
+  $('#work_count').append(min)
+}
+var disp_rest = function(ms) {
+  min = Math.floor(ms/60000)
+  $('#rest_count').append(min);
+}
 
-// Display work, rest, reps, and time
-	disp_work(work);
-	disp_rest(rest);
-	disp_reps(reps);
-	disp_timer(min, sec);
+// display main timer
+var disp_timer = function(ms) {
+  var min = Math.floor(ms/60000)
+  ms = ms - (min * 60000);
+  var sec = Math.floor(ms/1000)
+  $('#timer_count').empty();
+  $('#timer_count').append(add_zero(min) + ":" + add_zero(sec))
+}
 
-// add or remove work time
-	$('#work_minus').click( function() {
-		$('#work_count').empty();
-		if (work > 1) {
-		work = work - 1;
-		} else (work = 1);
-		disp_work(work);
-		timer = new Date(work);
-		sec = 0;
-		disp_timer(timer, sec)
-	})
-	$('#work_plus').click( function() {
-		$('#work_count').empty();
-		if (work < 60) {
-			work = work + 1
-		} else (work = 60);
-		disp_work(work);
-		timer = new Date(work);
-		sec = 0;
-		disp_timer(timer, sec)
-	})
+// ??display rest timer after main timer is finished
+var disp_rest_timer = function(min) {
+  $('#timer.count').empty();
+  $('#timer.count').append("R" + add_zero(min) + ":00")
+}
 
-// add or remove rest time
-	$('#rest_minus').click( function() {
-		$('#rest_count').empty();
-		if (rest > 1) {
-		rest = rest - 1;
-		} else (rest = 1);
-		disp_rest(rest);
-	})
-	$('#rest_plus').click( function() {
-		$('#rest_count').empty();
-		if (rest < 9) {
-			rest = rest + 1
-		} else (rest = 9);
-		disp_rest(rest);
-	})
+// add zero prefix for time display
+function add_zero(x) {
+  if (x < 10) {
+    x = "0" + x;
+  }
+  return x;
+}
 
-// add or remove reps
-	$('#reps_minus').click( function() {
-		$('#reps_count').empty();
-		if (reps > 1) {
-		reps = reps - 1;
-		} else (reps = 1);
-		disp_reps(reps);
-	})
-	$('#reps_plus').click( function() {
-		$('#reps_count').empty();
-		if (reps < 5) {
-			reps = reps + 1
-		} else (reps = 5);
-		disp_reps(reps);
-	})
+// returns time left between function call and end in ms
+function time_left(end) {
+  now = new Date();
+  if (now < end) {
+    remain = end - now;
+    disp_timer(remain);
+  } else {
+    // TODO: end timer, start break timer
+  }
+}
 
-// click start button, start timer
-	$('#start').one("click", function() {
-		var start_time = new Date();
-		var add_time = minutes_to_milliseconds(work);
-		var work_end_time = new Date(start_time.getTime() + add_time);
-		var add_more = minutes_to_milliseconds(rest);
-		var rest_end_time = new Date(work_end_time.getTime() + add_more);
-		//function displays timer during timing
-		var update_work_timer = function() {
-			// display time during work timer
-			if (new Date() < work_end_time) {
-				var time_left = work_end_time.getTime() - new Date();
-				min = new Date(time_left).getMinutes();
-				sec = new Date(time_left).getSeconds();
-				disp_timer(min, sec);
-			}
-		}
+// --- START DOCUMENT.READY ---
+$(document).ready(function() {  
+  // initilize work, rest and timer counts
+  var work = 120000; // 2 min in ms
+  var rest = 60000;  //1 min in ms
+  var sec = 0
+  disp_work(work);
+  disp_rest(rest);
+  disp_timer(work, sec);
 
-		// function displays timer during rest
-		var update_rest_timer = function() {
-			if (new Date() < rest_end_time) {
-				var time_left = rest_end_time.getTime() - new Date();
-				min = new Date(time_left).getMinutes();
-				sec = new Date(time_left).getSeconds();
-				disp_rest_timer(min, sec);
-			}
-		}
-		var start = function() {
-			if (new Date() >= work_end_time && new Date() < rest_end_time) {
-				$('#stop').click(function() {
-					rest_end_time = work_end_time;
-				})
-				var run_rest = setInterval(update_rest_timer, 100);
-			}
-			else if (new Date() < work_end_time) {
-				$('#stop').click(function() {
-					work_end_time = new Date(0);
-				})
-				var run_work = setInterval(update_work_timer, 100);
-			}
-		}
+  //add or remove work time
+  $('#work_minus').click(function() {
+    $('#work_count').empty();
+    if (work > 60000) {
+      work -= 60000;
+    } else (work = 60000)
+    disp_work(work);
+    disp_timer(work, sec);
+  })
+  $('#work_plus').click(function() {
+    $('#work_count').empty();
+    if (work < 3600000) {
+      work += 60000;
+    } else (work = 3600000)
+    disp_work(work);
+    disp_timer(work, sec);
+  })
 
-		var run = setInterval(start, 100);
-		$('#stop').click(function() {
-			clearInterval(run);
-			sec = 0;
-			disp_timer(work, sec);
-		})
-	})
+  //add or remove rest time
+  $('#rest_minus').click( function() {
+    $('#rest_count').empty();
+    if (rest > 60000) {
+    rest -= 60000;
+    } else (rest = 60000);
+    disp_rest(rest);
+    disp_rest_timer(rest)
+  })
+  $('#rest_plus').click( function() {
+    $('#rest_count').empty();
+    if (rest < 540000) {
+      rest = rest + 60000
+    } else (rest = 540000);
+    disp_rest(rest);
+    disp_rest_timer(rest);
+  })
+
+  // click start button
+  $('#start').on("click", function() {
+    var work_stop = new Date( new Date().getTime() + (work) + (sec) );
+    var running = setInterval(function() { time_left(work_stop) }, 100)
 })
 
+  // click stop button
+  $('#stop').on("click", function() {
+    console.log("stop button clicked")
+    // stop timer
+    clearInterval((function() { time_left(work_stop) })
+  })
+}); //end doc.ready
 
 
-// functions to display work, rest, rep, timer numbers
-var disp_work = function(min) {
-	$('#work_count').append(min);
-}
-var disp_rest = function(min) {
-	$('#rest_count').append(min);
-}
-var disp_reps = function(repetitions) {
-	$('#reps_count').append(repetitions);
-}
-var disp_timer = function(min, sec) {
-	min = minutes_to_milliseconds(min);
-	sec = sec * 1000;
-	$('#timer_count').empty();
-	$('#timer_count').append(addZero(new Date(min).getMinutes()) + ":" + addZero(new Date(sec).getSeconds()));
-}
-var disp_rest_timer = function(min, sec) {
-	min = minutes_to_milliseconds(min);
-	sec = sec * 1000;
-	$('#timer_count').empty();
-	$('#timer_count').append("R" +new Date(min).getMinutes() + ":" + addZero(new Date(sec).getSeconds()));
-}
-
-// convert minutes to milliseconds
-var minutes_to_milliseconds = function(min) {
-	var ms = min * 60 * 1000;
-	return ms;
-}
-
-// add zeros for time display
-var addZero = function(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
-}
-
-var repeat = function(fn, times) {
-	for (var i = 0; i < times; i++) fn();
-}
